fe21cfdb360f4d1da2b3d2981edddfd1c5c99b13
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-02-06 17:09:22 +0100

    added home-manager config

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -13,11 +17,24 @@
         config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
+      user = "raphaelw";
     in {
       nixosConfigurations = {
         basicConfig = lib.nixosSystem {
           inherit system;
           modules = [ ./configuration.nix ];
+        };
+      };
+      homeManagerConfig = {
+        userConfig = home-manager.lib.homeManagerConfiguration {
+          inherit system pkgs;
+          username = "${user}";
+          homeDirectory = "/home/${user}";
+          configuration = {
+            imports = [
+              ./home.nix
+            ];
+          };
         };
       };
     };
