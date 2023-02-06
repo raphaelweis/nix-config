b4755836a884c59e29e6879153836fa1fbdb17b9
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-02-06 16:50:20 +0100

    first commit

---
+++ configuration.nix
@@ -0,0 +1,89 @@
+{ config, pkgs, ... }:
+
+let
+  user = "raphaelw";
+  userFullName = "Raphaël Weis";
+in  
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
+    };
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Europe/Paris";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "fr_FR.UTF-8";
+      LC_IDENTIFICATION = "fr_FR.UTF-8";
+      LC_MEASUREMENT = "fr_FR.UTF-8";
+      LC_MONETARY = "fr_FR.UTF-8";
+      LC_NAME = "fr_FR.UTF-8";
+      LC_NUMERIC = "fr_FR.UTF-8";
+      LC_PAPER = "fr_FR.UTF-8";
+      LC_TELEPHONE = "fr_FR.UTF-8";
+      LC_TIME = "fr_FR.UTF-8";
+    };
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
+      libinput.enable = true;
+      layout = "us";
+      xkbVariant = "intl";
+    };
+    printing.enable = true;
+    openssh.enable = true;
+  };
+
+  sound.enable = true;
+  hardware = {
+    pulseaudio.enable = true;
+    bluetooth.enable = true;
+  };
+
+  users.users.${user} = {
+    description = "${userFullName}";
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    gh
+    firefox
+  ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  system = {
+    copySystemConfiguration = true;
+    stateVersion = "22.11";
+  };
+}
