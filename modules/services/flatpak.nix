663752530981a03849cb2dc2a7779d8bda27bd53
Raphaël Weis <raphael.weis.g@gmail.com>
2026-03-01 16:06:10 +0100

    feat: add discord as a flatpak

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -5,10 +5,20 @@
     imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
     services.flatpak = {
       enable = true;
+      overrides = {
+        global = {
+          Context.filesystems = [
+            "/nix/store:ro"
+          ];
+        };
+      };
       packages = [
         # pgadmin desktop with electron not available in nixpkgs so we are using the
         # flatpak for now. TODO: try to package pgadmin with electron
         "org.pgadmin.pgadmin4"
+
+        # Discord nix package has issues with krisp audio so we use the flatpak instead
+        "com.discordapp.Discord"
       ];
     };
   };
