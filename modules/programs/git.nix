618dc64667bead55591e380fc0cba270a0d49c3e
Raphaël Weis <raphael.weis.g@gmail.com>
2026-03-02 09:32:05 +0100

    feat: configure lazygit + delta pager

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,14 +1,33 @@
 {
   flake.modules.homeManager.git = {
-    programs.git = {
-      enable = true;
-      settings = {
-        user = {
-          email = "raphael.weis.g@gmail.com";
-          name = "Raphaël Weis";
+    # Use git, with lazygit as a TUI and delta as a diff viewer
+    programs = {
+      git = {
+        enable = true;
+        settings = {
+          user = {
+            email = "raphael.weis.g@gmail.com";
+            name = "Raphaël Weis";
+          };
+          pull.rebase = true;
+          push.autoSetupRemote = true;
         };
-        pull.rebase = true;
-        push.autoSetupRemote = true;
+      };
+      lazygit = {
+        enable = true;
+        enableZshIntegration = true;
+        settings = {
+          git.pagers = [
+            { pager = "delta --dark --paging=never"; }
+          ];
+        };
+      };
+      delta = {
+        enable = true;
+        enableGitIntegration = true;
+        options = {
+          syntax-theme = "gruvbox-dark";
+        };
       };
     };
   };
