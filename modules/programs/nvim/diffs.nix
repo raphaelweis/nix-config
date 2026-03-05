2200a41e49d0aa47004ad7fea741dfb7c777ecc0
Raphaël Weis <raphael.weis.g@gmail.com>
2026-03-05 10:13:28 +0100

    feat: update diffs.nvim

--- modules/programs/nvim/diffs.nix
+++ modules/programs/nvim/diffs.nix
@@ -9,8 +9,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "barrettruth";
             repo = "diffs.nvim";
-            rev = "749a21ae3c5c1f419120765357fdcce29374f24a";
-            hash = "sha256-BQfvqZX4DD1TcwHea6HxDOTPm2H+6JdaWlqCmgGorD0=";
+            rev = "7a3c4ea01e2ad53c6b54136bc19b7f0ad977da7d";
+            hash = "sha256-rsgboDQ7s9pIc+pDOJgwTA6c950CDY7gLsyn5oycsGI=";
           };
         })
       ];
