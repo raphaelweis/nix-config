a8ef5727dc03eb8dba490f6ecbe058b1f085504c
Raphaël Weis <raphael.weis.g@gmail.com>
2026-03-11 18:41:03 +0100

    feat: use diffs.nvim from nixpkgs-unstable instead of local package

--- modules/programs/nvim/diffs.nix
+++ modules/programs/nvim/diffs.nix
@@ -1,19 +1,14 @@
+{ inputs, ... }:
 {
   flake.modules.nixvimModules.diffs =
     { pkgs, ... }:
+    let
+      # this is an example of how to access a package in nixpkgs-unstable instead
+      # of using nixvim's default nixpkgs channel
+      pkgsUnstable = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    in
     {
-      extraPlugins = [
-        # TODO try to package in nixpkgs
-        (pkgs.vimUtils.buildVimPlugin {
-          name = "diffs.nvim";
-          src = pkgs.fetchFromGitHub {
-            owner = "barrettruth";
-            repo = "diffs.nvim";
-            rev = "7a3c4ea01e2ad53c6b54136bc19b7f0ad977da7d";
-            hash = "sha256-rsgboDQ7s9pIc+pDOJgwTA6c950CDY7gLsyn5oycsGI=";
-          };
-        })
-      ];
+      extraPlugins = [ pkgsUnstable.vimPlugins.diffs-nvim ];
       globals = {
         diffs = {
           fugitive = true;
